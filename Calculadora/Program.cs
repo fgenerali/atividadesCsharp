8742cc5dc877079f855c81b1a3708df713e7c379
Fernando Generali Engles <moitacia@hotmail.com>
2021-03-24 11:16:49 -0300

    primeiro commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Calculadora");
+                Console.WriteLine("Informe o primeiro número: ");
+                decimal n1 = Console.ReadLine().ToDecimal();
+
+                Console.WriteLine("Informe o segundo número: ");
+                decimal n2 = Console.ReadLine().ToDecimal();
+
+                Console.WriteLine("Informe a operação matemática");
+                Console.WriteLine(" + Somar \n - Subtrair \n * Multiplicação \n / Divisão \n % Resto divisão");
+                string operacao = Console.ReadLine();
+                Console.WriteLine("");
+
+                Console.WriteLine("Resultado: " + Calcular(n1, n2, operacao));
+                Console.ReadLine();
+            }           
+            catch(Exception erro)
+            {
+                Console.WriteLine("Operação ou valor informados inválido. \n" + erro);
+            }
+
+        }
+
+        public static decimal Calcular(decimal valor1, decimal valor2, string operacao)
+        {
+            if (operacao.Equals("+"))
+            {
+                return valor1 + valor2;
+            } else if (operacao.Equals("-"))
+            {
+                return valor1 - valor2;
+            } else if (operacao.Equals("*"))
+            {
+                return valor1 * valor2;
+            } else if (operacao.Equals("/"))
+            {   
+                if (valor2 != 0)
+                {
+                    return valor1 / valor2;
+                }
+                else
+                {
+                    return 0;
+                }
+                
+            } else
+            {
+                if (valor2 != 0)
+                {
+                    return (valor1 % valor2);
+                }
+                else
+                {
+                    return 0;
+                }
+                
+            }
+        }
+
+
+
+        
+    }
+    internal static class StringExtensions
+    {
+        internal static decimal ToDecimal(this string valor)
+        {
+            return decimal.Parse(valor);
+        }
+    }
+}
---
+++ FaixaEtaria/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaixaEtaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Exercício 2");
+            Console.WriteLine("Informe sua data de nascimento - Formato DD/MM/YYYY - Dia(2), Mês(2), Ano(4)");
+            DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+            int ano = dataNascimento.Year;
+            Console.WriteLine("Ano de nascimento: " + dataNascimento.ToString("dd/MM/yyyy") + "\nVocê é um " + calcularIdade(ano));
+            Console.ReadLine();
+        }
+
+
+
+
+        public static string calcularIdade(int ano)
+        {
+            int dataAtual = DateTime.Today.Year;
+            int idade = dataAtual - ano;
+            if(idade <= 19)
+            {
+                return "Jovem";
+            } else if(idade > 19 && idade < 60)
+            {
+                return "Adulto";
+            } else
+            {
+                return "Idoso";
+            }
+            
+        }
+    }
+}
